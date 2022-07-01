d8a0067df8f4f9bb162dba33ee6d13a644409ecf
Anda Toshiki <101481353+andatoshiki@users.noreply.github.com>
2022-07-02 05:55:34 +0800

    feat(geoip2): added geoip2 db full version support

--- internal/bindata/geoip/geoip.go
+++ internal/bindata/geoip/geoip.go
@@ -1,6 +1,6 @@
 // Code generated for package bingeoip by go-bindata DO NOT EDIT. (@generated)
 // sources:
-// assets/GeoLite2-City.mmdb
+// assets/GeoIP2-City.mmdb
 // assets/flags.json
 package bingeoip
 
@@ -26,10 +26,10 @@ type asset struct {
 	info  os.FileInfo
 }
 
-// assetsGeolite2CityMmdb reads file data from disk. It returns an error on failure.
-func assetsGeolite2CityMmdb() (*asset, error) {
-	path := "assets/GeoLite2-City.mmdb"
-	name := "assets/GeoLite2-City.mmdb"
+// assetsGeoIP2CityMmdb reads file data from disk. It returns an error on failure.
+func assetsGeoIP2CityMmdb() (*asset, error) {
+	path := "assets/GeoIP2-City.mmdb"
+	name := "assets/GeoIP2-City.mmdb"
 	bytes, err := bindataRead(path, name)
 	if err != nil {
 		return nil, err
@@ -114,7 +114,7 @@ func AssetNames() []string {
 
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
-	"assets/GeoLite2-City.mmdb": assetsGeolite2CityMmdb,
+	"assets/GeoIP2-City.mmdb": assetsGeoIP2CityMmdb,
 	"assets/flags.json":         assetsFlagsJson,
 }
 
@@ -160,7 +160,7 @@ type bintree struct {
 
 var _bintree = &bintree{nil, map[string]*bintree{
 	"assets": {nil, map[string]*bintree{
-		"GeoLite2-City.mmdb": {assetsGeolite2CityMmdb, map[string]*bintree{}},
+		"GeoIP2-City.mmdb": {assetsGeoIP2CityMmdb, map[string]*bintree{}},
 		"flags.json":         {assetsFlagsJson, map[string]*bintree{}},
 	}},
 }}
